bd25c1840ce2d3e8d23b5cf7c2d873f332ef240e
Juliana Praxedes <julianapraxeds@gmail.com>
2022-11-12 16:54:54 -0300

    Organizing script

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
-const container = document.getElementById('container')
-for (let i = 0; i < 1600; i++) {
-    const gridSq = document.createElement("div");
-    gridSq.classList.add('gridSq')
-    gridSq.style.width = '2.5%'
-    gridSq.style.height = '2.5%'
-    gridSq.style.border = '1px dotted black'
-    container.append(gridSq)
-}
-
-const gridSq = document.getElementsByClassName('gridSq')
-for (let i = 0; i < gridSq.length; i++) {
-    const square = gridSq[i]
-    square.addEventListener('mouseover', function () {
-        square.style.background = '#ff70a6'
-    })
-}
-
-const refreshBtn = document.getElementById('refreshBtn')
-refreshBtn.addEventListener('click', function () {
-    for (let i = 0; i < gridSq.length; i++) {
-        const square = gridSq[i]
-        square.style.background = 'transparent'
-    }
-})
-
 const settingsBtn = document.getElementById('settingsBtn')
 const settingsPopUpDiv = document.getElementById('settingsPopUp')
 const popUpBackground = document.getElementById('popUpBackground')
+const gridSq = document.getElementsByClassName('gridSq')
+
 
 
 settingsBtn.addEventListener('click', function () {
@@ -39,6 +15,24 @@ popUpBackground.addEventListener('click', function () {
     popUpBackground.classList.toggle('noDisplay')
 })
 
+
+const setBtn = document.getElementById('setBtn')
+setBtn.addEventListener('click', function () {
+    createGrid()
+    settingsPopUpDiv.classList.toggle('noDisplay')
+    popUpBackground.classList.toggle('noDisplay')
+    
+})
+
+const refreshBtn = document.getElementById('refreshBtn')
+refreshBtn.addEventListener('click', function () {
+    for (let i = 0; i < gridSq.length; i++) {
+        const square = gridSq[i]
+        square.style.background = 'transparent'
+    }
+})
+
+
 const sqSlider = document.getElementById('sqSlider')
 var sqSliderOutput = document.getElementById('sqSliderOutput')
 sqSliderOutput.innerHTML = `${sqSlider.value} x ${sqSlider.value}`
@@ -46,16 +40,8 @@ sqSlider.oninput = function () {
     sqSliderOutput.innerHTML = `${this.value} x ${this.value}`
 }
 
-let gridSqSize
-
-const setBtn = document.getElementById('setBtn')
-setBtn.addEventListener('click', function () {
-    settingsPopUpDiv.classList.toggle('noDisplay')
-    popUpBackground.classList.toggle('noDisplay')
-    //!100% dividido pelo numero do input value pra saber o tamanho do width e height de cada quadrado em porcentagem
-    //!definir o tamanho dos quadrados de acordo com a divisao
-    //!resultado x resultado pra saber quantos quadrados cabem no container
-    gridSqSize = 100 / sqSlider.value
+function createGrid(){
+    let gridSqSize = 100 / sqSlider.value
     containerAreaSq = sqSlider.value * sqSlider.value
     for (let i = 0; i < containerAreaSq; i++) {
         const gridSq = document.createElement("div");
@@ -70,7 +56,6 @@ setBtn.addEventListener('click', function () {
     }
     if (gridSq.length > containerAreaSq) {
         const sqDifference = gridSq.length - containerAreaSq
-        console.log(sqDifference)
         for (let i = 0; i < sqDifference; i++) {
             gridSq[i].remove()
         }
@@ -79,8 +64,13 @@ setBtn.addEventListener('click', function () {
         const square = gridSq[i]
         square.style.background = 'transparent'
     }
-
-})
+    for (let i = 0; i < gridSq.length; i++) {
+        const square = gridSq[i]
+        square.addEventListener('mouseover', function () {
+            square.style.background = '#ff70a6'
+        })
+    }
+}
 
 const colorOptions = document.getElementById('colorOptions')
 colorOptions.addEventListener('click', function (e) {
@@ -94,3 +84,19 @@ colorOptions.addEventListener('click', function (e) {
         })
     }
 })
+
+createGrid()
+
+
+
+// const container = document.getElementById('container')
+// for (let i = 0; i < 1600; i++) {
+//     const gridSq = document.createElement("div");
+//     gridSq.classList.add('gridSq')
+//     gridSq.style.width = '2.5%'
+//     gridSq.style.height = '2.5%'
+//     gridSq.style.border = '1px dotted black'
+//     container.append(gridSq)
+// }
+
+
